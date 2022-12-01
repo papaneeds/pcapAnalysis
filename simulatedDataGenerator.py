65d6391fac87f944d4849fa1226edf36e58fcce9
Tom Szeredi <papaneeds@gmail.com>
2022-12-01 05:52:00 -0500

    created simulated data

--- simulatedDataGenerator.py
+++ simulatedDataGenerator.py
@@ -2,6 +2,9 @@
 from dateutil import parser
 import datetime
 import random
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 timeStartString = '2022-12-01T09:00:00.000000' # ISO 8601 format
 timeStart = parser.parse(timeStartString)
@@ -11,15 +14,19 @@ timeInterval = 60 # units of seconds. This is how often we write out the metrics
 currentTime = timeStart
 
 secondsPerHour = 3600
-deltaMax = 20.0
+deltaMax = 30.0
+maxVal = 100
+minVal = 15
+numIntervals = 20
+randomAmplitude = deltaMax * 0.01
 
 t_0 = currentTime.timestamp() # seconds since 1970
-delta_t = random.random()*timeEndAfterStart # hours for change
-t_1 = t_0 + delta_t*secondsPerHour
+delta = random.random()*timeEndAfterStart/numIntervals # hours for change
+t_1 = t_0 + delta*secondsPerHour
 
-delta = random.random()*deltaMax
+period = math.pi/(delta*secondsPerHour)
 m = delta/(t_1 - t_0)
-y_0 = 10.0
+y_0 = 22.0
 sign = random.choice([-1, 1])
 y = y_0 + sign*delta*(t_1 - t_0)/secondsPerHour
 
@@ -27,24 +34,50 @@ print("t_0=", t_0, " t_1=", t_1, " delta=", delta, " m=", m, " y_0=", y_0, " sig
 print("y=", y)
 keepGoing = True
 
+x_arr = []
+y_arr = []
+
 while (currentTime < timeEnd):
+
     t = currentTime.timestamp()
-    y = y_0 + sign*delta*(t - t_0)/secondsPerHour
+    randomAmount = math.sin(period*(t-t_0))
+    y = y_0 + sign*delta*(t - t_0)/secondsPerHour + randomAmount
     print("currentTime=", currentTime, " t=", t, " y=", y)
+    if (y > maxVal):
+        y = maxVal
+    elif (y < minVal):
+        y = minVal
+    x_arr.append(currentTime.isoformat())
+    y_arr.append(y)
 
     if (t > t_1):
         # Reset the slopes, end time, etc
         t_0 = currentTime.timestamp() # seconds since 1970
-        delta_t = random.random()*timeEndAfterStart # hours for change
+        delta_t = random.random()*timeEndAfterStart/numIntervals # hours for change
         t_1 = t_0 + delta_t*secondsPerHour
 
         delta = random.random()*deltaMax
         m = delta/(t_1 - t_0)
         sign = random.choice([-1, 1])
+        y_0 = y
     
     currentTime = currentTime + datetime.timedelta(seconds=timeInterval)
 
+print(x_arr, y_arr)
+plt.plot(x_arr, y_arr)
+plt.show()
 
+user_input = input("Do you want to save this graph? (y/n)")
+if (user_input == 'y' or user_input == 'Y'):
+    user_input = input("Enter the filename to write data to ")
+    f = open(user_input, 'w')
+    outpuString = "timestamp, memory\n"
+    f.write(outpuString)
+    for i in range(len(x_arr)):
+        outputString = str(x_arr[i]) + "," + str(y_arr[i]) + "\n"
+        f.write(outputString)
+
+    f.close()
 
 
 
